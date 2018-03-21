0362cc42b8e16fdff9d873d557ed94b9a8e35122
cravxx <environskrakow@protonmail.com>
2018-03-20 21:00:36 -0500

    hacky way to read windows tags

---
+++ file_tags.py
@@ -0,0 +1,15 @@
+import re
+
+def get_tags(matched_file):
+    with open(matched_file, "rb") as imageFile:
+            d = imageFile.read()
+            #xmp_start = d.find(b'<x:xmpmeta')
+            xmp_start = d.find(b'<MicrosoftPhoto:LastKeywordXMP>')
+            #xmp_end = d.find(b'</x:xmpmeta')
+            end_token = b'</MicrosoftPhoto:LastKeywordXMP>'
+            xmp_end = d.find(end_token)
+            if xmp_start is not -1 and xmp_end is not -1:
+                xmp_str = d[xmp_start:xmp_end+len(end_token)]
+                return re.findall(b'(?s)(?<=<rdf:li>).*?(?=<\/rdf:li>)', xmp_str)
+            else:
+                return None
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fnmatch import fnmatch
 from datetime import datetime
 from util import write_tag
 from color import get_colors
+from file_tags import get_tags
 
 color_exclude_list = [ "*.webm", "*.mp4", "*.gif", "*.gifv" ]
 
@@ -13,7 +14,8 @@ _DIR = os.getcwd() #"image"
 for matched_file in [f for f in os.listdir(_DIR) if not fnmatch(f, "*.txt")]:
     try:
         with open(os.path.join(_DIR, matched_file) + ".txt", "wb") as text_file:
-
+            
+            #colors
             if not any(fnmatch(matched_file, pattern) for pattern in color_exclude_list):
                 image_colors = get_colors(os.path.join(_DIR, matched_file))
                 if image_colors is not None:
@@ -21,6 +23,12 @@ for matched_file in [f for f in os.listdir(_DIR) if not fnmatch(f, "*.txt")]:
                     print("[",matched_file,"] matched colors: ",image_colors[0][0],image_colors[1][0])
                     write_tag(text_file, "color", image_colors[0][0])
                     write_tag(text_file, "color", image_colors[1][0])
+
+            #windows tags
+            meta_tags = get_tags(os.path.join(_DIR, matched_file))
+            if meta_tags is not None:
+                for tag in meta_tags:
+                    write_tag(text_file, "metatag", tag.decode('utf-8'))
 
             d = datetime.strptime(time.ctime(os.path.getmtime(os.path.join(_DIR, matched_file))),"%a %b %d %H:%M:%S %Y")
             write_tag(text_file, "modified", d.strftime('%Y/%m/%d %H:%M:%S'))
